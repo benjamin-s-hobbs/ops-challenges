377b7122390f06459c15831f33019ac8b70de3c2
Benjamin <benjamin.s.hobbs@gmail.com>
2023-04-27 05:09:06 +0000

    committing

---
+++ loginhistory.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge: Class03
+# Author:                       Ben Hobbs
+# Date of latest revision:      04/26/2023
+# Purpose:                      print the login history of users on this computer
+
+# Declaration of variables
+var="This is the login history"
+# Declaration of functions
+print_something () {
+    last
+    echo $var
+}
+
+# Main:                          
+print_something ()
+print_something ()
+print_something ()
+
+
+
+
+
+# End
