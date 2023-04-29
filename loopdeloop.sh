9e210e36600b1a630fac1a1e1db5c334c2e7929f
Benjamin <benjamin.s.hobbs@gmail.com>
2023-04-29 19:13:30 +0000

    "final save till class"

--- loopdeloop.sh
+++ loopdeloop.sh
@@ -11,25 +11,25 @@
 
 
 # Declaration of functions
+
 run_joshua () {
-# write a script that displays running processes
-ps aux
-# asks the user for a PID
+ps 
 echo "What PID number would you like to choose?"
 read var1
-# kills the process with that PID
 kill $var1
-echo "Thank you Professor Falken, PID $var1 has been destroyed. Resuming DEFCON1"
+echo "Thank you Professor Falken, PID ${var1} has been destroyed. Resuming DEFCON1"
 # Loops (while loop) back to step 1 and continues until user exits with Ctrl + C
-while [1=1]
+while [ "x${var1}" > x0 ]
 do
-ps aux
-echo "What PID number would you like to choose?"
-read var1
-kill $var1
-echo "Thank you Professor Falken, PID $var1 has been destroyed. Resuming DEFCON1"
+    ps 
+    echo "What PID number would you like to choose?"
+    read var1
+    kill ${var1}
+    echo "Thank you Professor Falken, PID ${var1} has been destroyed. Resuming DEFCON1"
 done
+
 echo "An interesting game. The only winning move is not to play. How about a nice game of chess?"
+
 }
 
 # Main:                          
