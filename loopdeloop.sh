6ebbe76fe6faf4ab44239706f3e5a4d57e6be6af
Benjamin <benjamin.s.hobbs@gmail.com>
2023-04-29 16:36:26 +0000

    wish me luck

---
+++ loopdeloop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge: Class05
+# Author:                       Ben Hobbs
+# Date of latest revision:      04/28/2023
+# Purpose:                      To write a script that displays running processes, asks the user for a PID, and then kills the process with that PID;
+#                               while using a loop in your script
+
+
+# Declaration of variables
+# Put the names of the four directories in an array
+
+
+# Declaration of functions
+#Creates four directories: dir1, dir2, dir3, dir4
+mkdir_new () { 
+  mkdir dir1
+  touch ${bigbox[3]}/paper.txt 
+}
+# Reference the array variable to create a new .txt file in each directory
+
+# Main:                          
+
+# End
