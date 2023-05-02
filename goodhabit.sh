b9c7774fef801afe3b2d20053a779c22fb1f5487
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-02 00:48:55 +0000

    making6

---
+++ goodhabit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge: Class05
+# Author:                       Ben Hobbs
+# Date of latest revision:      04/28/2023
+# Purpose:                      To write a script that displays running processes, asks the user for a PID, and then kills the process with that PID;
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main:                          
+
+# End
---
+++ ifthengo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge: Class05
+# Author:                       Ben Hobbs
+# Date of latest revision:      05/1/2023
+# Purpose:                      Create a script that detects if a file or directory exists, then creates it if it does not exist.
+#                               Your script must use at least one array, one loop, and one conditional.
+
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main:                          
+
+# End
