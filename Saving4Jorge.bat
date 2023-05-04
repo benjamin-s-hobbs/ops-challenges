162d723c591a55760ccdba5584733f5ec2355b4f
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-04 18:02:10 +0000

    adding our script framework to this

--- Saving4Jorge.bat
+++ Saving4Jorge.bat
@@ -1,1 +1,20 @@
-robocopy C:\Users\j.thompson\Desktop E:\AutoDesktop *.* /rh:0000-0200 /s /z /dcopy:date /log+ jorgelog.txt
+:: Windows Batch Scripting
+::
+
+:: Script Name:                  Ops Challenge: Class08
+:: Author:                       Ben Hobbs
+:: Date of latest revision:      05/4/2023
+:: Purpose:                      Create a batch file that recursively copies Jorge’s work files on his desktop (recursively meaning to include folder contents).
+
+
+:: Declaration of variables
+
+
+:: Declaration of functions
+
+
+:: Main:
+
+robocopy C:\Users\j.thompson\Desktop\*.* E:\AutoDesktop /rh:0000-0200 /s /z /dcopy:date /log+: jorgelog.txt
+
+:: End
