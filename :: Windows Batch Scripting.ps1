4d7c479a61e39e4d0fc6b207f6248dca858b39f3
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-05 07:00:34 +0000

    Powershell a-gogo!

--- :: Windows Batch Scripting.ps1
+++ :: Windows Batch Scripting.ps1
@@ -1,4 +1,5 @@
-:: Windows Batch Scripting
+::Eventloggin 
+::Windows Batch Scripting
 
 :: Script Name:                  Ops Challenge: Class09
 :: Author:                       Ben Hobbs
@@ -13,8 +14,20 @@
 :: Main:  
 
 :: Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
+$Begin = Get-Date -Date '5/4/2023 09:00:00'
+$End = Get-Date -Date '5/5/2023 09:00:00'
+Get-EventLog -LogName System -After $Begin -Before $End | Out-File -Path \\Desktop\last_24.txt
+
 :: Output all “error” type events from the System event log to a file on your desktop named errors.txt.
+Get-EventLog -LogName System -EntryType Error | Out-File -Path \\Desktop\errors.txt
+
 :: Print to the screen all events with ID of 16 from the System event log.
+Get-EventLog -LogName -EventID 16 
+
 :: Print to the screen the most recent 20 entries from the System event log.
+Get-EventLog -LogName System -Newest 20
+
+:: Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text)
+Get-EventLog -LogName System -Newest 500 
 
 :: End
