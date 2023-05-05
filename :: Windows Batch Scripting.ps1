4a4eef146131e26f83294ee40cc9b7516d0bc952
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-05 06:05:30 +0000

    first powershell commit

---
+++ :: Windows Batch Scripting.ps1
@@ -0,0 +1,20 @@
+:: Windows Batch Scripting
+
+:: Script Name:                  Ops Challenge: Class09
+:: Author:                       Ben Hobbs
+:: Date of latest revision:      05/04/2023
+:: Purpose:                      Write a set of Powershell commands that fetch useful System event logs 
+::                               and include language appropriate syntax and file extensions.
+
+:: Declaration of variables
+
+:: Declaration of functions
+
+:: Main:  
+
+:: Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt.
+:: Output all “error” type events from the System event log to a file on your desktop named errors.txt.
+:: Print to the screen all events with ID of 16 from the System event log.
+:: Print to the screen the most recent 20 entries from the System event log.
+
+:: End
