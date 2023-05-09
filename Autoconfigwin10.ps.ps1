75e020b2368f55dd55017922d3089c74a5be2a5f
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-09 04:19:06 +0000

    here we are. Class11 commit

---
+++ Autoconfigwin10.ps.ps1
@@ -0,0 +1,39 @@
+Autoconfigwin10.ps.1
+
+# Script Name:                  Ops Challenge: Class11 (Autoconfigwin.ps1)
+# Author:                       Ben Hobbs
+# Date of latest revision:      05/08/2023
+# Purpose:                      Write a Powershell script that automates the configuration of a new Windows 10 endpoint. Your script should perform the following:
+#                               Enable File and Printer Sharing, Allow ICMP traffic, Enable Remote management, Remove bloatware
+#                               Enable Hyper-V, Disable SMBv1, an insecure protocol
+
+# Declaration of variables
+
+# Declaration of functions
+
+# Main:                          
+
+#Write a Powershell script that automates the configuration of a new Windows 10 endpoint. 
+#Your script should perform the following:
+
+
+#Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True 
+
+#Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4 
+
+#Enable Remote management
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+#Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+#Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+#Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+
+# End
