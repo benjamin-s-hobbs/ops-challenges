bb5b061d9b8f5c28eccfa17b5b9a658c6fc376c1
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-11 02:07:23 +0000

    CC13 new commit

---
+++ domainintel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge: Class13
+# Author:                       Ben Hobbs
+# Date of latest revision:      05/10/2023
+# Purpose:                      Create a script that asks a user to type a domain, then displays information about the typed domain. 
+#                               Operations that must be used include: whois, dig, host, nslookup
+
+
+
+# Declaration of variables
+
+
+# Declaration of functions
+
+
+# Main:                          
+
+# End
