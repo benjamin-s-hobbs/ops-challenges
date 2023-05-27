6cb50742a99425d36193c52001a04bf9942803fa
Benjamin <benjamin.s.hobbs@gmail.com>
2023-05-27 13:51:31 +0000

    adding 201 folder. moving files to it for cleanup. adding first 301 file in python. first commit with new title.

---
+++ hello.py
@@ -0,0 +1,19 @@
+#hello.py
+
+# Script Name:                  Ops Challenge: Prep (hello.py)
+# Author:                       Ben Hobbs
+# Date of latest revision:      05/27/2023
+# Purpose:                      To write a script that displays running processes, asks the user for a PID, and then kills the process with that PID;
+
+
+# Declaration of variables
+
+msg = "Hello World"
+
+# Declaration of functions
+
+# Main:                          
+
+print(msg)
+
+# End
