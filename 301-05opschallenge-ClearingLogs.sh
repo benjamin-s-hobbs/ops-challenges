e024045825bef88f813514c0fa72a9262b4a7a4f
Benjamin <benjamin.s.hobbs@gmail.com>
2023-06-06 05:56:24 +0000

    Updating 301-05opschallenge-ClearingLogs.sh

--- 301-05opschallenge-ClearingLogs.sh
+++ 301-05opschallenge-ClearingLogs.sh
@@ -15,24 +15,32 @@
 #                               Compare the size of the compressed files to the size of the original log files
 
 # Declaration of variables
+
 varlogfat1=(~/var/log/syslog)
 varlogfat2=(~/var/log/wtmp)
+varlogbk=(~/var/log/backups/$varwhen)
+varwhen=(date +"syslog-%Y%m%d%H%M%S")
 
 # Declaration of functions
 
 fade_away() {
 
+# Print to the screen the file size of the log files before compression
 ls -lh * $varlogfat1
 ls -lh * $varlogfat2
+ 
+# Compress the contents of the log files listed below to a backup directory
+# The file name should contain a time stamp with the following format -YYYYMMDDHHMMSS
+# Example: /var/log/backups/syslog-20220928081457.zip
+zip $varlogfat1 >> ~/$varlogbk
+zip $varlogfat2 >> ~/$varlogbk
 
+# Clear the contents of the log file
+truncate -s 0 ~/$varlogfat1
+truncate -s 0 ~/$varlogfat2
 
-
-
-
-
-
-
-
+# Print to screen the file size of the compressed file
+ls -lh * ~/$varlogbk
 
 }
 
