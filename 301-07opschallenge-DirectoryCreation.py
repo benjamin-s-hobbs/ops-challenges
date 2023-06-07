e0ef9e2f9d8d6a9093cc685dd032fa45e69c7e1a
Benjamin <benjamin.s.hobbs@gmail.com>
2023-06-07 22:52:47 +0000

    Adding 301-07opschallenge

---
+++ 301-07opschallenge-DirectoryCreation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# the shebang line instructs the system to use the env command to locate the python3 interpreter and execute the script with it
+
+# Script Name:                  Ops Challenge: 301Class07opschallenge-DirectoryCreation.py
+# Author:                       Ben Hobbs
+# Date of latest revision:      06/7/2023
+# Purpose:                      Create a Python script that generates all directories, sub-directories and files for a user-provided directory path.
+#                               Script must ask the user for a file path and read a user input string into a variable.
+#                               Script must use the os.walk() function from the os library.
+#                               Script must enclose the os.walk() function within a python function that hands it the user input file path.
+
+# Import Libraries
+import os
+
+# Declaration of variables
+# How to declare a variable in python
+#   greeting = "Welcome to Python!"
+# How to call a variable
+#   print(greeting)
+
+
+
+# Declaration of functions
+
+
+
+
+# Main:
+                     
+
+
+
+# End
