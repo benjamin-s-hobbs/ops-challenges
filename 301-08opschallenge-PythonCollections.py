528e5979ba8ea69c5cf8b448966cc858e33e5633
Benjamin <benjamin.s.hobbs@gmail.com>
2023-06-09 04:48:01 +0000

    Adding 301-08opschallenge

---
+++ 301-08opschallenge-PythonCollections.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# the shebang line instructs the system to use the env command to locate the python3 interpreter and execute the script with it
+
+# Script Name:                  Ops Challenge: 301Class08opschallenge-PythonCollections.py
+# Author:                       Ben Hobbs
+# Date of latest revision:      06/8/2023
+# Purpose:                      Create a Python script that includes the following:
+#                               Assign to a variable a list of ten string elements.
+#                               Print the fourth element of the list.
+#                               Print the sixth through tenth element of the list.
+#                               Change the value of the seventh element to “onion”
+
+
+# Import Libraries
+import os
+
+# Declaration of variables
+# How to declare a variable in python
+#   greeting = "Welcome to Python!"
+# How to call a variable
+#   print(greeting)
+
+
+
+# Declaration of functions
+
+
+
+
+
+# Main:
+                     
+
+
+
+# End
