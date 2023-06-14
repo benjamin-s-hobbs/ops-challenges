8e6c5b879d93da3f7685b63e76b5f8fb1022acf1
Benjamin <benjamin.s.hobbs@gmail.com>
2023-06-14 20:23:25 +0000

    updating 301-11opschallenge-Psutil.py -splitting out the commands

--- 301-11opschallenge-Psutil.py
+++ 301-11opschallenge-Psutil.py
@@ -36,9 +36,23 @@ import psutil
 
 # Declaration of functions:
 
-
-print(psutil.cpu_times())
-
+print("Time spent by normal processes executing in user mode","\n", print(psutil.cpu_times().user))
+# Time spent by processes executing in kernel mode
+print(psutil.cpu_times().system)
+# Time when system was idle
+print(psutil.cpu_times().idle)
+# Time spent by priority processes executing in user mode
+print(psutil.cpu_times().nice)
+# Time spent waiting for I/O to complete.
+print(psutil.cpu_times().iowait)
+# Time spent for servicing hardware interrupts
+print(psutil.cpu_times().irq)
+# Time spent for servicing software interrupts
+print(psutil.cpu_times().softirq)
+# Time spent by other operating systems running in a virtualized environment
+print(psutil.cpu_times().steal)
+# Time spent running a virtual CPU for guest operating systems under the control of the Linux kernel
+print(psutil.cpu_times().guest)
 
 # Main:
 
