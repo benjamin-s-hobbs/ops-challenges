fe35a5398479462c59e6510dd83bdba69422da32
Benjamin Hobbs <benjamin.s.hobbs@gmail.com>
2023-07-19 06:25:02 +0000

    Updating Read 401-06

--- 401challenges/401-06opschallenge-FileEncryption.py
+++ 401challenges/401-06opschallenge-FileEncryption.py
@@ -5,7 +5,25 @@
 # Script Name:                  Ops Challenge: 401-06opschallenge-FileEncryption.py
 # Author:                       Ben Hobbs
 # Date of latest revision:      07/17/2023
-# Purpose:                      
+# Purpose:                      In Python, create a script that utilizes the cryptography library to:
+#                               Prompt the user to select a mode:
+#                               Encrypt a file      (mode 1)
+#                               Decrypt a file      (mode 2)
+#                               Encrypt a message   (mode 3)
+#                               Decrypt a message   (mode 4)
+#                               If mode 1 or 2 are selected, prompt the user to provide a filepath to a target file.
+#                               If mode 3 or 4 are selected, prompt the user to provide a cleartext string.
+#                               Depending on the selection, perform one of the below functions. 
+#
+#                               You’ll need to create four functions:
+#                               Encrypt the target file if in mode 1.
+#                               Delete the existing target file and replace it entirely with the encrypted version.
+#                               Decrypt the target file if in mode 2.
+#                               Delete the encrypted target file and replace it entirely with the decrypted version.
+#                               Encrypt the string if in mode 3.
+#                               Print the ciphertext to the screen.
+#                               Decrypt the string if in mode 4.
+#                               Print the cleartext to the screen.
 
 # References:
 
@@ -14,60 +32,69 @@
 from cryptography.fernet import Fernet
 
 # Declaration of variables (global)
+# Value of key is assigned to a variable
+f = Fernet(key)
+# The plaintext is converted = ciphertext
+token =f.encrypt(b"welcome to cybersecurity")
+
+# Decrypting the ciphertext = plaintext
+d = f.decrypt(token)
 
 
 
 
 # Declaration of functions
 
-# Write a function that handles key generation
-def gen_key():
+# Write a function to encrypt a message
+def encrypt_message():
 
     # Generate a key.
     key = Fernet.generate_key()
+
     # Saving the generated key to a file for reuse.
     with open("key.key", "wb") as key_file:
-        Key_file.write(key)      
-# Write a function to load up the generated key so we can encrypt and decrypt
-def load_key():
+        key_file.write(key)      
 
-    # Retreives the generated key from the key.key file.
-    return open("key.key", "r").read()
-# Function to encrypt a message
+    # Write a function to load up the generated key so we can encrypt and decrypt
+    def load_key():
 
-# Function to decrypt a message
+        # Retreives the generated key from the key.key file.
+        return open("key.key", "rb").read()
+    # Function to encrypt a message
 
-# Function to encrypt a file
+    # Function to decrypt a message
 
-encrypted_file = f.encrypt(file.data)
-# Function to decrypt a file
-def 
-# Function to create a user menu for the above options
+    # Function to encrypt a file
 
-# Main:
-# Check to see if a key exists already
+    encrypted_file = f.encrypt(file.data)
+    # Function to decrypt a file
+    
+    # Function to create a user menu for the above options
 
-# Generate and write the new key
-gen_key()
+    # Main:
+    # Check to see if a key exists already
 
-# Load the generated key
-key = load_key()
-print("Key is " + str(key.decode('utf-8')))
+    # Generate and write the new key
+    key = Fernet.generate_key()
+
+    # Load the generated key
+    key = load_key()
+    print("Key is " + str(key.decode('utf-8')))
 
 
 
-# Message to be encrypted
-message = "THIS IS TOP SECRET!!"
+    # Message to be encrypted
+    message = "THIS IS TOP SECRET!!"
 
-print ("Plaintext message is" + message)
+    print ("Plaintext message is" + message)
 
-# DO THE ENCRYPTION - Initiatilize the Fernet modeule and names it "f"
-f = Fernet(key)
+    # DO THE ENCRYPTION - Initiatilize the Fernet modeule and names it "f"
+    f = Fernet(key)
 
-#Encrypt your message
-encrypted_message = f.encrypt(message)
+    #Encrypt your message
+    encrypted_message = f.encrypt(message)
 
-#Print the encrypted message
-print()
+    #Print the encrypted message
+    print()
 
-# End
+    # End
