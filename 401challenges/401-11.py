417639e15f08a1a07117bdfadf07f69619bafd35
Benjamin Hobbs <benjamin.s.hobbs@gmail.com>
2023-07-25 06:18:58 +0000

    Updating 401-11.py

---
+++ 401challenges/401-11.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env/python3
+# the shebang line instructs the system to use the env command to locate the python3 
+# interpreter and execute the script with it
+
+# Script Name:                  Ops Challenge: 401-11-NetworkSecurityToolScapy1.py
+# Author:                       Ben Hobbs
+# Date of latest revision:      07/24/2023
+# Purpose:                      In Python, create a TCP Port Range Scanner that tests whether a TCP port is open or closed. The script must:
+#                               Utilize the scapy library
+#                               Define host IP
+#                               Define port range or specific set of ports to scan
+#                               Test each port in the specified range using a for loop
+#                               If flag 0x12 received, send a RST packet to graciously close the open connection. 
+#Notify the user the port is open.
+#                               If flag 0x14 received, notify user the port is closed.
+#                               If no flag is received, notify the user the port is filtered and silently dropped.
+
+
+# References:
+
+
+# Import Libraries:
+
+
+# Declaration of variables (Global):
+# How to declare a variable in python
+#   greeting = "Welcome to Python!"
+# How to call a variable
+#   print(greeting)
+
+
+
+# Declaration of functions:
+
+
+# Main (calling functions):
+
+
+
+# End (end of script)
