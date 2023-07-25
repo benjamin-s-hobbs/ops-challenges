76eaf92b0a644b5494bb9b5cb5db28f722f28656
Benjamin Hobbs <benjamin.s.hobbs@gmail.com>
2023-07-25 16:13:13 +0000

    401-11.py

--- 401challenges/401-11.py
+++ 401challenges/401-11.py
@@ -62,27 +62,30 @@ print(response)
 # Write a function to test our specified range of ports (for loop)
 
 # Write a function to send a RST packet if 0x12 flag is received (to close the open connection)
-if (response.haslayer(TCP)):
-    #Gets the value of the flag in the TCP packet. Compares it to 0x12 (SYN-ACK)
-    if (response.getlayer(TCP).flags == 0x12):
-        # Then the port is open. Send a RST packet to close it.
-        send_rst = sr1(IP(dst=host)/TCP(sport=scr_port,dport=dst_port,flags="S"),timeout=1, verbose=0)
-        # Notify the user that the port is open.
-        print(f"{host}:{dst_port} is open.")
-        print(host + str(dst_port) + " is open.")
-    # Gets the value of the flag in the TCP packet. Compares it to 0x14 (RST)
-    elif (response.getlayer(TCP).flags == 0x14):
-        # Then the port is closed. Notify user the port is closed.
-        print(f"{host}:{dst_port} is closed.")
-        print(host + str(dst_port) + " is closed.")
-    # Gets the value of the flag in the TCP packet. No flag in the TCP packet?
-    else (response.getlayer(TCP).flags == 0x00):
-        # if no flag is received, notify the user the port is filtered and siliently dropped.
-        print(f"{host}:{dst_port} is filtered and silently dropped.")
-        print(host + str(dst_port) + " is filtered and silently dropped.")
+def scan_ports():
+    if (response.haslayer(TCP)):
+        #Gets the value of the flag in the TCP packet. Compares it to 0x12 (SYN-ACK)
+        if (response.getlayer(TCP).flags == 0x12):
+            # Then the port is open. Send a RST packet to close it.
+            send_rst = sr1(IP(dst=host)/TCP(sport=scr_port,dport=dst_port,flags="S"),timeout=1, verbose=0)
+            # Notify the user that the port is open.
+            print(f"{host}:{dst_port} is open.")
+            print(host + str(dst_port) + " is open.")
+        # Gets the value of the flag in the TCP packet. Compares it to 0x14 (RST)
+        elif (response.getlayer(TCP).flags == 0x14):
+            # Then the port is closed. Notify user the port is closed.
+            print(f"{host}:{dst_port} is closed.")
+            print(host + str(dst_port) + " is closed.")
+        # Gets the value of the flag in the TCP packet. No flag in the TCP packet?
+        else (response.getlayer(TCP).flags == 0x00):
+            # if no flag is received, notify the user the port is filtered and siliently dropped.
+            print(f"{host}:{dst_port} is filtered and silently dropped.")
+            print(host + str(dst_port) + " is filtered and silently dropped.")
 
 
 # Main (calling functions):
+
+scan_ports()
 
 
 
