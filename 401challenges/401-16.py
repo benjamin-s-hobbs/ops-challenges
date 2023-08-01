e78204653ba633094a093f3ca932bd939462db9c
Benjamin Hobbs <benjamin.s.hobbs@gmail.com>
2023-08-01 13:01:11 +0000

    Updating 401-16.py

--- 401challenges/401-16.py
+++ 401challenges/401-16.py
@@ -35,8 +35,60 @@ import getpass
 
 # Declaration of functions:
 
+# Write a function to accept input of a word list
+def dict_iterator():
+    filepath = input("Enter your dictionary filepath:\n")
+    #filepath = '/home/osboxes/Desktop/rockyou2.txt' #test filepath
+    # Iterate through the word list
+    file = open(filepath, encoding = "ISO-8859-1") # address encoding problem
+    # Assign the word being read to a variable
+    line = file.readline()
+    while line:
+        line = line.rstrip()
+        word = line
+        # Print the value of the variable to the screen.
+        print(word)
+        # Add a delay between words (1 sec)
+        time.sleep(1)
+        line = file.readline()
+    file.close()
 
+# Write a function to accept input of a string
+
+# Search the word list for the user input string.
+# Print the result of the search to the screen.
+def check_password():
+    # Define a string to a variable
+    password= input("What password would you like to check?\n")
+    # Accept a user input word list file path
+    filepath = input("Enter your dictionary filepath:\n")
+    search = 
+    print(search)
+
+# Write a function to combine these options into a user menu
+def BForce_tool():
+    if __name__ == "__main__": # when my computer runs this file...do this stuff
+        while True:
+            # Call the first function "Mode 1: Offensive; Dictionary Iterator"
+            # Call the second function "Mode 2: Defensive; Password Recognized"
+            mode = input("""
+    Brute Force Wordlist Attack Tool Menu
+    1 - Offensive, Dictionary Iterator                         
+    2 - Defensive, Password Recognized
+    3 - Exit
+        Please enter a number: 
+    """)
+            if (mode == "1"):
+                dict_iterator()
+            elif (mode == "2"):
+                check_password()
+            elif (mode == '3'):
+                break
+            else:
+                print("Invalid selection...I'm sorry, you must be at least 18 years old or at least know how numbers work to use this tool")
 
 # Main (call your functions)
+
+BForce_tool()
 
 # End (end of script)
