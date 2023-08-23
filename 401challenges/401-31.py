ae0cff08f599c0501f32c6c21f4bf0bd5d9ea645
Benjamin Hobbs <benjamin.s.hobbs@gmail.com>
2023-08-23 06:54:30 +0000

    Creating 401-32.py

---
+++ 401challenges/401-31.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env/python3
+# the shebang line instructs the system to use the env command to locate the python3 
+# interpreter and execute the script with it
+
+# Script Name:                  Ops Challenge: 401-31.py Signature-based Malware Detection Part 1 of 3
+# Author:                       Ben Hobbs
+# Date of latest revision:      08/21/2023
+# Purpose:                      Ops Challenge: Signature-based Malware Detection Part 1 of 3
+#                               In Python, write a script that will:
+#                               Prompt the user to type in a file name to search for.
+#                               Prompt the user for a directory to search in.
+#                               Search each file in the directory by name.
+#                               TIP: You may need to perform different commands depending on what OS you’re executing the script on.
+#                               For each positive detection, print to the screen the file name and location.
+#                               At the end of the search process, print to the screen how many files were searched and how many hits were found.
+#                               The script must successfully execute on both Ubuntu Linux 20.04 Focal Fossa and Windows 10.
+
+
+# References:
+# Demo code Class31
+# Marco Vazquez 401-Class31 topic intro 
+# Marco Vazquez 401-Class32 topic review
+# 
+
+# Import Libraries:
+import os
+
+# Declaration of variables (Global):
+
+# Declaration of functions:
+
+
+# Main (calling functions):
+
+
+
+
+# End (end of script)
---
+++ 401challenges/401-32.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env/python3
+# the shebang line instructs the system to use the env command to locate the python3 
+# interpreter and execute the script with it
+
+# Script Name:                  Ops Challenge: 401-32.py Signature-based Malware Detection Part 2 of 3
+# Author:                       Ben Hobbs
+# Date of latest revision:      08/22/2023
+# Purpose:                      Ops Challenge: Signature-based Malware Detection Part 2 of 3
+#                               In Python, Continue developing your Python malware detection tool.
+#                               Alter your search code to recursively scan each file and folder in the user input directory path and print it to the screen.
+#                               For each file scanned within the scope of your search directory:
+#                               Generate the file’s MD5 hash using Hashlib.
+#                               Assign the MD5 hash to a variable.
+#                               Print the variable to the screen along with a timestamp, file name, file size, and complete (not relative) file path.
+#                               
+#                               TIP: You may need to bring in additional Python modules to complete today’s objective.
+#                               The script should be tested to execute successfully in Python3.
+
+
+# References:
+# Demo code Class32
+# Marco Vazquez 401-Class32 topic intro 
+# 
+# 
+
+# Import Libraries:
+import os
+import hashlib
+
+# Declaration of variables (Global):
+
+# Declaration of functions:
+
+
+# Main (calling functions):
+
+
+
+
+# End (end of script)
